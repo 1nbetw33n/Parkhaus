89d46ee4edf933220d00ca0ad52779a1b5fcf384
1nbetw33n <a.ab@mail.de>
2021-05-28 14:11:07 +0200

    Created junit test for regular expressions and created methods with empty bodies.

---
+++ src/main/personal_spaces/bella/exercise6/no2/testRegEx.java
@@ -0,0 +1,61 @@
+package main.personal_spaces.bella.exercise6.no2;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+final class testRegEx {
+
+    @BeforeEach
+    void setUp(){
+    }
+
+
+    @AfterEach
+    void tearDown(){
+    }
+
+
+    @Test
+    @DisplayName("test if regular expression for zip's work like intended")
+    void testRegExZip(){
+    }
+
+
+    @Test
+    @DisplayName("test if regular expression for phone numbers work like intended")
+    void testRegExPhoneNumber(){
+    }
+
+
+    @Test
+    @DisplayName("test if regular expression for license plates work like intended")
+    void testRegExLicensePlate(){
+    }
+
+
+    @Test
+    @DisplayName("test if regular expression for mail addresses work like intended")
+    void testRegExZipMailAddress(){
+    }
+
+
+    @Test
+    @DisplayName("test if regular expression for URLs work like intended")
+    void testRegExURL(){
+    }
+
+
+    @Test
+    @DisplayName("test if regular expression for java dev comments work like intended")
+    void testRegExZipJavaDevComment(){
+    }
+
+
+    @Test
+    @DisplayName("test if regular expression for java class names work like intended")
+    void testRegExZipJavaClassName(){
+    }
+
+}
