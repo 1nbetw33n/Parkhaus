459618254937c3af52e1aeb5ee4c0ca1c9b3a6d2
1nbetw33n <a.ab@mail.de>
2021-05-28 15:45:49 +0200

    Implemented interface RegExable in RegEx. Implemented empty bodied methods.

--- src/main/personal_spaces/bella/exercise6/no2/RegEx.java
+++ src/main/personal_spaces/bella/exercise6/no2/RegEx.java
@@ -1,5 +1,38 @@
-
 package main.personal_spaces.bella.exercise6.no2;
 
-public class RegEx {
+public class RegEx implements RegExable{
+
+    public boolean regExZip() {
+        return false;
+    }
+
+
+    public boolean regExPhoneNumber() {
+        return false;
+    }
+
+
+    public boolean regExLicensePlate() {
+        return false;
+    }
+
+
+    public boolean regExZipMailAddress() {
+        return false;
+    }
+
+
+    public boolean regExURL() {
+        return false;
+    }
+
+
+    public boolean regExZipJavaDevComment() {
+        return false;
+    }
+
+
+    public boolean regExZipJavaClassName() {
+        return false;
+    }
 }
